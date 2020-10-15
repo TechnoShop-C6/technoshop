5b89b88df6df3cda9a198da336bfee6a45c3ebc9
unknown <heni.oussama23@gmail.com>
2020-10-15 20:17:13 +0100

    added some style to navbar and buttons

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,49 @@
 import React from "react";
 import Login from "./Login.jsx";
 import SignIn from "./SignIn.jsx";
+import Laptop from './Laptop.jsx';
+import Phone from './Phone.jsx';
+import Accessory from './Accessory.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      user :''
+    }
   }
+
+ 
 
   render() {
     return (
-      <div>
-        <Login />
-
-
-
-
-        <SignIn />
+        <div>
+          <div className="navbar">
+          <span className="logo"
+            onClick={() => this.changeView('feed')}>
+            TECHNOSHOP💻
+          </span>
+             <span className="nav" onClick={() => this.changeView("Laptop")}>
+             Laptop
+            </span>
+            <span className="nav" onClick={() => this.changeView("Phone")}>
+            {" "}
+            Phone
+           </span>
+           <span className="nav" onClick={() => this.changeView("Accessory")}>
+           {" "}
+           Accessory
+          </span>
+          <div className="leftnav">
+            <span className="nav" onClick={() => this.changeView("Profile")}>
+              {" "}
+             Profile
+             </span>
+          </div>
+        </div>
+        <div className="main" > 
+        <Accessory />
+        </div>
       </div>
     );
   }
--- client/src/components/Phone.jsx
+++ client/src/components/Phone.jsx
@@ -2,21 +2,50 @@ import React from 'react';
 
 
 class Phone extends React.Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
+        this.state={
+            data : []
+        }
     }
 
 
 
+    componentDidMount(){
+        const options = {
+          method: 'get',
+          url: '/product'
+        };
+        axios(options)
+          .then(products=> {
+            this.setState({
+              data: products.data
+            });
+          })
+          .catch(error => {
+            console.error(error);
+          })
+        }
+    
+
 
     render(){
+        const listOfPhone = this.state.data.filter((item) => item.category === 'phone').map ((product) => ( 
+         <div key={product.id}>
+            <img src={product.url} className="post-image"/>
+            <h3>{product.name}</h3>
+            <h3>{product.category}</h3>
+            <h3>{product.price}</h3>
+            <p>{product.property}</p>
+            <button>Edit</button>
+            <button>Delete</button>
+            <button>Add to Chart</button>
+
+         </div>))
         return(
             <div>
-               <img src="" className="post-image"/>
-               <h3>name</h3>
-               <h3>category</h3>
-               <h3>price</h3>
-               <p>property</p>
+               {listOfPhone}
+
             </div>
         )
     }
